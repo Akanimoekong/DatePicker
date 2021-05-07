88df84eab3c7f9d31d6fbdceb045c49dbbfef4a5
mac <kingsley.usoro@tellerium.com>
2021-05-07 13:35:26 +0100

    Added my own implementation for calendar

--- app/src/main/java/com/fsi/datepickerkotlin/MainActivity.kt
+++ app/src/main/java/com/fsi/datepickerkotlin/MainActivity.kt
@@ -1,55 +1,50 @@
 package com.fsi.datepickerkotlin
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-
 import android.app.DatePickerDialog
 import android.os.Bundle
-import android.widget.DatePicker
+import android.util.Log
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
-class MainActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener {
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val calendar: Calendar = Calendar.getInstance()
-
         val button = findViewById<EditText>(R.id.date_fromEdt)
-        val dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH)
-        val month = calendar.get(Calendar.MONTH)
-        val year = calendar.get(Calendar.YEAR)
+
 
         val button2 = findViewById<EditText>(R.id.date_toEdt)
-        val dayOfMonth2 = calendar.get(Calendar.DAY_OF_MONTH)
-        val month2 = calendar.get(Calendar.MONTH)
-        val year2 = calendar.get(Calendar.YEAR)
+        val textViewFrom = findViewById<TextView>(R.id.date_from)
+        val textViewTo = findViewById<TextView>(R.id.date_to)
 
         button.setOnClickListener {
-            val datePickerDialog =
-                DatePickerDialog(this, this, year, month, dayOfMonth)
-            datePickerDialog.show()
+            showDatePicker { year, monthOfYear, day ->
+                Log.i(TAG, "$year $monthOfYear $day")
+            }
         }
 
         button2.setOnClickListener {
-            val datePickerDialog =
-                DatePickerDialog(this, this, year2, month2, dayOfMonth2)
-            datePickerDialog.show()
+            showDatePicker { year, monthOfYear, day ->
+                Log.i(TAG, "$year $monthOfYear $day")
+            }
         }
     }
-    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
-        val textView = findViewById<TextView>(R.id.date_from)
-        val myDay = dayOfMonth
-        val myYear = year
-        val myMonth = month +1
-        textView.text = "Year: $myYear \n Month: $myMonth  \n Day: $myDay"
 
-        val textView2 = findViewById<TextView>(R.id.date_to)
-//        val myDay2 = dayOfMonth2
-//        val myYear2 = year2
-//        val myMonth2 = month2 +1
-//        textView2.text = "Year: $myYear2 \n Month: $myMonth2  \n Day: $myDay2"
+    private fun showDatePicker(onDateSelected: (year: Int, monthOfYear: Int, day: Int) -> Unit) {
+        val calendar: Calendar = Calendar.getInstance()
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+        val currentMonth = calendar.get(Calendar.MONTH)
+        val currentYear = calendar.get(Calendar.YEAR)
+        DatePickerDialog(this, { _, year, monthOfYear, dayOfMonth ->
+            onDateSelected.invoke(year, monthOfYear, dayOfMonth)
+
+        }, currentYear, currentMonth, day).show()
     }
 }
